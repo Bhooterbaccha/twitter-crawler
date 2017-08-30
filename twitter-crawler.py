f64987994b08325c3cabef138aaf4cad343c53ab
Avirup Mukherjee <Avimukh.250696@gmail.com>
2017-08-30 20:27:30 +0530

    Update twitter-crawler.py
    twitter-crawler.py modified

--- twitter-crawler.py
+++ twitter-crawler.py
@@ -8,7 +8,6 @@ api = tweepy.API(auth)
 
 #file input as command to write them in the file
 fil=sys.argv[1]
-
 data ={}
 class MyStreamListener(tweepy.StreamListener):
     i=1
@@ -16,16 +15,24 @@ class MyStreamListener(tweepy.StreamListener):
         try:    
             oFile = open(fil,"a")
             data['name'] = status.user.name
+            #data['screen_name'] = status.user.screen_name optional
             data['location'] = status.user.location
             data['text'] = status.text
-            data['created_at'] = status.created_at
-            s=str(MyStreamListener.i) #counts number of filtered tweets obtained
+            data['created_at'] = status.created_at 
+            #data['geo'] = status.coordinates optional
+            #data['source'] = status.source optional
+            s=str(MyStreamListener.i)
             data_location = str(data['location'])
             data_text = str(data['text']).encode('utf-16', 'ignore').decode('utf-16','ignore')
             data_time = str(data['created_at'])
             if "Houston" in data_text and "flood" in data_text:
                 oFile.write(data_text+">>>>"+data_location+">>>>"+data_time+">>>>"+s+"\n")
                 MyStreamListener.i +=1
+
+        except:
+            pass
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-myStream.filter(track=['Houston flood']) #any search term to filter tweets as per the need.
+myStream.filter(track=['Houston' and 'flood'])
+oFile=open(fil)
+oFile.close()
